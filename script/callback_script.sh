363229577b6641775c395bf1a0f2cdff3cfd7b83
ozoramore <dev@t2arc.net>
2026-04-20 12:17:38 +0900

    Change comments to English

--- script/callback_script.sh
+++ script/callback_script.sh
@@ -2,5 +2,5 @@
 # original is https://zenn.dev/iberianpig/articles/seamless_switching_between_vim_and_tui
 
 gitroot="$(git rev-parse --show-superproject-working-tree --show-toplevel | head -1)"
-echo edit "$1 $gitroot/$2" > /tmp/tig_callback # $1が+行番号 $2がファイルパス
-kill -1 $PPID # SIGHUPでtigを終了させる
+echo edit "$1 $gitroot/$2" > /tmp/tig_callback # $1: line number, $2: file path
+kill -1 $PPID #  terminate tig with SIGHUP.
